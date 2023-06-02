d8c0745cc5df7f146eef2a32a0d6602f6ce142ac
Gonzalo Núñez <gonzalo.nunez1801@alumnos.ubiobio.cl>
2023-06-01 22:41:09 -0400

    Cambios menores

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const recetaRoute = require('./routes/recetas.routes')
 
 const app = express()
 const dotenv = require('dotenv')
@@ -8,7 +9,7 @@ dotenv.config()
 // MIDDLEWARES
 app.use(cors())
 app.use(express.json())
-
+app.use('/api', recetaRoute)
 // ERROR HANDLING
 app.use((err, req, res, next) => {
   return res.status(500).json({
