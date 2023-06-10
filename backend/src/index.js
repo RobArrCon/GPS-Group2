b2b348781a079fd2447aa09daa166fe0764b05d3
Jajaz16 <jazmin.contreras1801@alumnos.ubiobio.cl>
2023-06-10 01:00:24 -0400

    revisioncontrolador

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,8 +5,9 @@ const UsuarioRoutes = require('./routes/usuario.routes')
 const ingredienteRoutes = require('./routes/ingredientes.routes')
 const listaRoutes = require('../src/routes/lista.routes')
 const postRoutes = require('../src/routes/post.routes')
-const CategoriaRoutes = require('./routes/Categoria.Routes')
+const CategoriaRoutes = require('./routes/categoria.routes')
 const opinionRoutes = require('./routes/opinion.routes')
+const productoRoutes = require('./routes/producto.routes')
 const app = express()
 const dotenv = require('dotenv')
 dotenv.config()
@@ -23,6 +24,8 @@ app.use('/api', listaRoutes)
 app.use('/api', postRoutes)
 app.use('/api', CategoriaRoutes)
 app.use('/api', opinionRoutes)
+app.use('/api', productoRoutes)
+
 // ERROR HANDLING
 app.use((err, req, res, next) => {
   return res.status(500).json({
