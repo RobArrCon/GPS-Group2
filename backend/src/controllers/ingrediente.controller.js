264c54ca576bcd26f8c4fb58abbc80d5fa7d4e39
Unachascaloca <bryanruizc@hotmail.com>
2023-07-13 22:44:18 -0400

    llamen a dios

--- backend/src/controllers/ingrediente.controller.js
+++ backend/src/controllers/ingrediente.controller.js
@@ -2,9 +2,9 @@ const pool = require('../db')
 
 const createIngrediente = async (req, res, next) => {
   try {
-    const { nombre, descripcion, categoria } = req.body
+    const { nombre, descripcion } = req.body
     const query = await pool.query('INSERT INTO ingrediente (nombre_ingrediente,descripcion_ingrediente,categoria_ingrediente) VALUES($1,$2,$3) RETURNING *',
-      [nombre, descripcion, categoria])
+      [nombre, descripcion])
     res.status(200).json(query.rows[0])
   } catch (error) {
     next(error)
@@ -49,10 +49,10 @@ const deleteIngrediente = async (req, res, next) => {
 
 const updateIngrediente = async (req, res, next) => {
   try {
-    const { codigo, nombre, descripcion, categoria } = req.body
+    const { codigo, nombre, descripcion } = req.body
     const query = await pool.query(
       'UPDATE ingrediente SET nombre_ingrediente = $2, descripcion_ingrediente = $3, categoria_ingrediente = $4 WHERE codigo_ingrediente = $1 RETURNING *',
-      [codigo, nombre, descripcion, categoria])
+      [codigo, nombre, descripcion])
     if (query.rowCount === 0) {
       return res.status(404).json({ message: 'Ingrediente no encontrado' })
     }
