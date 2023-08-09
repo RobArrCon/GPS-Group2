324588111e62429fd8ec44898e85950ce3a16f8b
Cristina Tiznado <41882365+CristinaTiznado@users.noreply.github.com>
2023-08-09 03:37:48 -0400

    Update Register.jsx

--- frontend/pages/Register.jsx
+++ frontend/pages/Register.jsx
@@ -11,9 +11,9 @@ import {
   LinearProgress
 } from '@mui/material'
 import Head from 'next/head'
-import MyBackground2 from '../components/Background-2.jsx'
-import MyButton from '../components/Button.jsx'
-import ImagenV from '../components/Vaquita.jsx'
+import MyBackground2 from '../components/LorellanaComponents/Background-2.jsx'
+import MyButton from '../components/LorellanaComponents/Button.jsx'
+import ImagenV from '../components/LorellanaComponents/Vaquita.jsx'
 
 const Register = () => {
   const router = useRouter()
